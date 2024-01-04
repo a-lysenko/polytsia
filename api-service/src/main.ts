ce6a9582575053f4d6048ff9f07f4fb495e21e8d
Oleksii Lysenko <aleksey.lysenko@gmail.com>
2024-01-04 22:27:11 +0200

    - api-service: add Dockerfile

--- api-service/src/main.ts
+++ api-service/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { env } from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,6 +21,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(env.PORT || 3000);
 }
 bootstrap();
